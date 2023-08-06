de57fa81f134047441882d05a89b3f9816df1d6b
jeanmax1me <jeanmaximegilbert@gmail.com>
2023-08-06 21:45:34 +0200

    small fix for deployment

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -10,13 +10,20 @@ import { useProModal } from "@/hooks/use-pro-modal";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export const ProModal = () => {
   const proModal = useProModal();
   const { toast } = useToast();
   const [loading, setLoading] = useState(false);
+const [isMounted, setIsMounted] = useState(false);
+
+useEffect(() => {
+  setIsMounted(true);
+}, [])
+
+
   const onSubscribe = async () => {
     try {
       setLoading(true);
@@ -32,6 +39,10 @@ export const ProModal = () => {
       setLoading(false);
     }
   };
+
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
